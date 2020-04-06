1f5eaecaf5ddd21b16ae2bef531ba35f9d208be3
KUDO Shunsuke <kudo.shunsuke.xx@gmail.com>
2020-04-06 17:05:45 +0900

    up

--- k8s-vue-svg/app/vue-svg/src/components/SvgComponent.vue
+++ k8s-vue-svg/app/vue-svg/src/components/SvgComponent.vue
@@ -1,22 +1,28 @@
 <template>
-  <div class="svgarea"
-	>
-    <svg width="400" height="400" 
-			@mouseleave="moveend"
-			@mousemove="over" 
-		>
-      <rect class="rect1" width="200" height="50" rx="8" ry="8" x="0" y="0"
-				@mouseup="moveend" 
-				@mousedown="movestart" 
-			>
-      </rect>
-      <rect class="rect2" width="200" height="50" rx="8" ry="8" x="50" y="50"
-				@mouseup="moveend" 
-				@mousedown="movestart" 
-			>
-      </rect>
+  <div class="svgarea">
+    <svg width="400" height="400"
+      @mouseleave="moveend"
+      @mousemove="over"
+      @mouseup="moveend"
+    >
+      <g
+        @mousedown="movestart"
+      >
+        <rect class="rect1" width="200" height="50" rx="8" ry="8" x="0" y="0"></rect>
+        <text x="5" y="15">
+          aaaa
+        </text>
+      </g>
+
+      <g
+        @mousedown="movestart"
+      >
+        <rect class="rect2" width="200" height="50" rx="8" ry="8" x="0" y="0"></rect>
+        <text x="5" y="15">
+          bbbb
+        </text>
+      </g>
     </svg>
-		<br/>
   </div>
 </template>
 
@@ -26,57 +32,63 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class SvgComponent extends Vue {
   private isMousedown!: bool;
-	private view: any;
-	private svg: any;
-	private target: any;
+  private view: any;
+  private svg: any;
+  private target: any;
+  private targetgroup: any;
   private mousex: number;
   private mousey: number;
   private targetdx: number;
   private targetdy: number;
 
   constructor() {
-	  super()
-	  this.isMousedown = false
-		this.mousex = 0
-		this.mousey = 0
+    super()
+    this.isMousedown = false
+    this.mousex = 0
+    this.mousey = 0
   }
 
   mounted() {
-		console.log(this.$el)
-		// keep svg element
-		this.view = this.$el
-		this.svg = this.$el.children[0]
-	}
+    console.log(this.$el)
+    // keep svg element
+    this.view = this.$el
+    this.svg = this.$el.children[0]
+  }
 
   private over(ev) {
-		const dx = this.view.getBoundingClientRect().left
-		const dy = this.view.getBoundingClientRect().top
-		this.mousex = ev.clientX -dx
-		this.mousey = ev.clientY -dy
-		if (!this.isMousedown) {
-			return
-		}
+    const dx = this.view.getBoundingClientRect().left
+    const dy = this.view.getBoundingClientRect().top
+    this.mousex = ev.clientX -dx
+    this.mousey = ev.clientY -dy
+    if (!this.isMousedown) {
+      return
+    }
 
-		this.target.setAttribute('x',this.mousex - this.targetdx)
-		this.target.setAttribute('y',this.mousey - this.targetdy)
+    this.targetgroup.setAttribute('transform', `translate(${this.mousex - this.targetdx}, ${this.mousey - this.targetdy})`)
   }
 
   private move(ev) {
-		console.log("move")
-		console.log(ev)
+    console.log("move")
+    console.log(ev)
   }
+
   private movestart(ev) {
-		console.log("movestart")
-		this.target = ev.target
-		this.svg.appendChild(ev.target)
-		console.log(this.target.style)
-		this.targetdx = ev.offsetX
-		this.targetdy = ev.offsetY
-		this.isMousedown = true
+    console.log("movestart")
+    this.target = ev.target
+    console.log(ev)
+    console.log(ev.offsetX, ev.offsetY)
+    this.targetgroup = ev.target.parentNode
+    this.svg.appendChild(ev.target.parentNode)
+    this.targetdx = ev.offsetX
+    this.targetdy = ev.offsetY
+    this.isMousedown = true
   }
+
   private moveend(ev) {
-		console.log("moveend")
-		this.isMousedown = false
+    console.log("moveend")
+    this.isMousedown = false
+    this.target = null
+    this.targetgroup = null
   }
 }
 </script>
@@ -85,9 +97,9 @@ export default class SvgComponent extends Vue {
 rect{
   stroke-width: 5px;
   cursor: pointer;
-	z-index: 0;
-	position: relative;
+  position: relative;
 }
+
 rect:hover{
   fill: #EFE;
 }
@@ -96,14 +108,15 @@ rect:hover{
   fill: white;
   stroke: #793;
 }
+
 .rect2{
   fill: white;
   stroke: #F93;
 }
 .svgarea {
-	border: 1px solid #000;
-	width: 400px;
-	height: 400px;
+  border: 1px solid #000;
+  width: 400px;
+  height: 400px;
 }
 
 .eye-outer{
