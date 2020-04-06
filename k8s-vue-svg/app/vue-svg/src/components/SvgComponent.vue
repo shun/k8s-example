2574a83772aba2a8d07e05c1d3385c283ee6da4d
KUDO Shunsuke <kudo.shunsuke.xx@gmail.com>
2020-04-06 23:24:35 +0900

    up

--- k8s-vue-svg/app/vue-svg/src/components/SvgComponent.vue
+++ k8s-vue-svg/app/vue-svg/src/components/SvgComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="svgarea">
-    <svg width="400" height="400"
+    <svg width="800" height="400" viewBox="0 0 800 400"
       @mouseleave="moveend"
       @mousemove="over"
       @mouseup="moveend"
@@ -8,19 +8,83 @@
       <g
         @mousedown="movestart"
       >
-        <rect class="rect1" width="200" height="50" rx="8" ry="8" x="0" y="0"></rect>
-        <text x="5" y="15">
-          aaaa
-        </text>
+        <rect class="rect1" width="240" height="80" rx="8" ry="8" x="0" y="0"></rect>
+        <g>
+          <text class="header_title" text-anchor="middle" x="120" y="20">mst_role</text>
+        </g>
+        <g>
+          <g>
+            <text class="primary_key" text-anchor="start" x="10"  y="50">id</text>
+            <text text-anchor="stop"  x="120" y="50">INTEGER</text>
+            <text class="right-edge"  text-anchor="stop"  x="230" y="50">|</text>
+          </g>
+
+          <g>
+            <text text-anchor="start" x="10"  y="70">name</text>
+            <text text-anchor="stop"  x="120" y="70">VARCHAR(30)</text>
+            <text class="right-edge"  text-anchor="stop"  x="230" y="70">|</text>
+          </g>
+
+        </g>
       </g>
 
       <g
         @mousedown="movestart"
       >
-        <rect class="rect2" width="200" height="50" rx="8" ry="8" x="0" y="0"></rect>
-        <text x="5" y="15">
-          bbbb
-        </text>
+        <rect class="rect2" width="240" height="80" rx="8" ry="8" x="0" y="0"></rect>
+        <g>
+          <text class="header_title" text-anchor="middle" x="120" y="20">role</text>
+        </g>
+        <g>
+          <g>
+            <text class="primary_key" text-anchor="start" x="10"  y="50">role_id</text>
+            <text text-anchor="stop"  x="120" y="50">INTEGER</text>
+            <text class="right-edge"  text-anchor="stop"  x="230" y="50">|</text>
+          </g>
+
+          <g>
+            <text class="primary_key" text-anchor="start" x="10"  y="70">user_id</text>
+            <text text-anchor="stop"  x="120" y="70">INTEGER</text>
+            <text class="right-edge"  text-anchor="stop"  x="230" y="70">|</text>
+          </g>
+
+        </g>
+      </g>
+
+      <g
+        @mousedown="movestart"
+      >
+        <rect class="rect1" width="200" height="120" rx="8" ry="8" x="0" y="0"></rect>
+        <g>
+          <text class="header_title" text-anchor="middle" x="100" y="20">mst_user</text>
+        </g>
+        <g>
+          <g>
+            <text class="primary_key" text-anchor="start" x="10" y="50">id</text>
+            <text class="right-edge"  text-anchor="stop"  x="190" y="50">|</text>
+          </g>
+
+          <g>
+            <text text-anchor="start" x="10" y="70">name</text>
+            <text class="right-edge"  text-anchor="stop"  x="190" y="70">|</text>
+          </g>
+
+          <g>
+            <text text-anchor="start" x="10" y="90">mail</text>
+            <text class="right-edge"  text-anchor="stop"  x="190" y="90">|</text>
+          </g>
+
+          <g>
+            <text text-anchor="start" x="10" y="110">order</text>
+            <text class="right-edge"  text-anchor="stop"  x="190" y="110">|</text>
+          </g>
+
+        </g>
+      </g>
+
+      <g>
+        <path stroke="#5d4037" stroke-width="2" fill="none" d="M250,120 C300,120 300,200 350,200"/>
+        <path stroke="#5d4037" stroke-width="2" fill="none" d="M600,180 C700,180 700,70 650,70"/>
       </g>
     </svg>
   </div>
@@ -115,8 +179,22 @@ rect:hover{
 }
 .svgarea {
   border: 1px solid #000;
-  width: 400px;
+  width: 800px;
   height: 400px;
+}
+
+.primary_key {
+  font-weight: bolder;
+  fill: #424242;
+}
+
+.header_title {
+  font-weight: bolder;
+  fill: #b0bec5;
+}
+
+.right-edge {
+  font-weight: bolder;
 }
 
 .eye-outer{
@@ -124,7 +202,10 @@ rect:hover{
   stroke: #793;
   stroke-width: 5px;
 }
+
 text{
+  fill: #616161;
+  font-weight: 100;
   cursor: pointer;
   pointer-events: none;
 }
